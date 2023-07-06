28bcf4fc821223893aeb64eeef002c0ee5e9f4ea
Panuwat Chinpratan <panuwatchinpratan@gmail.com>
2023-07-06 16:12:34 +0700

    good

--- src/data.jsx
+++ src/data.jsx
@@ -9,23 +9,10 @@ import {
 } from "react-icons/fa";
 import { FiFileText, FiUser, FiExternalLink } from "react-icons/fi";
 
-import Work1 from "./assets/project-1.jpeg";
+import Work1 from "./assets/project-1.jpg";
 import Work2 from "./assets/project-2.jpg";
-import Work3 from "./assets/project-3.jpeg";
-import Work4 from "./assets/project-4.jpeg";
-import Work5 from "./assets/project-5.jpg";
-import Work6 from "./assets/project-6.jpg";
+import Work3 from "./assets/project-3.jpg";
 
-import Theme1 from "./assets/purple.png";
-import Theme2 from "./assets/red.png";
-import Theme3 from "./assets/blueviolet.png";
-import Theme4 from "./assets/blue.png";
-import Theme5 from "./assets/goldenrod.png";
-import Theme6 from "./assets/magenta.png";
-import Theme7 from "./assets/yellowgreen.png";
-import Theme8 from "./assets/orange.png";
-import Theme9 from "./assets/green.png";
-import Theme10 from "./assets/yellow.png";
 
 export const links = [
   {
@@ -139,7 +126,7 @@ export const resume = [
     category: "experience",
     icon: <FaBriefcase />,
     year: "Present",
-    title: "inexperienced </span>",
+    title: "•inexperienced </span>",
     desc: "",
   },
 
@@ -149,7 +136,7 @@ export const resume = [
     icon: <FaGraduationCap />,
     year: "2021",
     title: "King Mongkut's Institute of Technology Ladkrabang </span>",
-    desc: "Bachelor of Science in Plant Production Technology",
+    desc: "•Bachelor of Science in Plant Production Technology",
   },
 
   {
@@ -158,7 +145,7 @@ export const resume = [
     icon: <FaGraduationCap />,
     year: "2017",
     title: "Nawaminthrachinuthit Suankularb Wittayalai Pathumthani </span>",
-    desc: "High School Diploma",
+    desc: "•High School Diploma",
   },
 ];
 
@@ -312,64 +299,3 @@ export const portfolio = [
   },
 ];
 
-export const themes = [
-  {
-    id: 1,
-    img: Theme1,
-    color: "hsl(252, 35%, 51%)",
-  },
-
-  {
-    id: 2,
-    img: Theme2,
-    color: "hsl(4, 93%, 54%)",
-  },
-
-  {
-    id: 3,
-    img: Theme3,
-    color: "hsl(271, 76%, 53%)",
-  },
-
-  {
-    id: 4,
-    img: Theme4,
-    color: "hsl(225, 73%, 57%)",
-  },
-
-  {
-    id: 5,
-    img: Theme5,
-    color: "hsl(43, 74%, 49%)",
-  },
-
-  {
-    id: 6,
-    img: Theme6,
-    color: "hsl(339, 81%, 66%)",
-  },
-
-  {
-    id: 7,
-    img: Theme7,
-    color: "hsl(80, 61%, 50%)",
-  },
-
-  {
-    id: 8,
-    img: Theme8,
-    color: "hsl(19, 96%, 52%)",
-  },
-
-  {
-    id: 9,
-    img: Theme9,
-    color: "hsl(88, 65%, 43%)",
-  },
-
-  {
-    id: 10,
-    img: Theme10,
-    color: "hsl(42, 100%, 50%)",
-  },
-];
